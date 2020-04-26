fcfd73fa56ac769dfb436fa86940c95712223c69
Bedford West <bedfordwest@gmail.com>
2020-04-26 01:09:36 -0500

    refactor(contacts): Refactor contact_handler

--- systems/physics/collisions/collisions.rb
+++ systems/physics/collisions/collisions.rb
@@ -1,11 +1,6 @@
 module Collisions
     def check_collisions(world)
         world.bodies.combination(2).each { |body1, body2|
-            if(body2.entity.is_a? Player)
-                center1 = body1.shape.center
-                center2 = body2.shape.center
-                #puts "#{body1.entity.class} at [#{center1.x},#{center1.y}] and #{body2.entity.class} at [#{center2.x},#{center2.y}]"
-            end
             if (body1.collides body2)
                 world.add_contact(body1, body2)
             end
--- systems/physics/collisions/contact_handler.rb
+++ systems/physics/collisions/contact_handler.rb
@@ -6,25 +6,26 @@ class ContactHandler
     include PlayerContacts
 
     def initialize
-        @contacts = Array.new #will store all of the contacts for the handler to check
+        @body_contacts = Array.new #will store all of the body contacts for the handler to check
+        @fixture_contacts = Array.new #will store all of the fixture contacts for the handler to check
     end
 
     #iterate through the contacts and perform updates to entities accordingly
     def update(delta)
-        @contacts.each { |contact|
-            case contact.obj_2
-            when Body
-                case contact.obj_2.entity
-                when Player
-                    player_contact(contact)
-                end
-            when Fixture
-                @contacts << Contact.new(contact.obj_1.body, contact.obj_2.body)
+        @body_contacts.each { |contact|
+            case contact.obj_2.entity
+            when Player
+                player_contact(contact)
             end
         }
     end
 
     def add_contact(contact)
-        @contacts << contact
+        if(contact.obj_1).is_a? Body
+            @body_contacts << contact
+        else
+            @fixture_contacts << contact
+            @body_contacts << Contact.new(contact.obj_1.body, contact.obj_2.body)
+        end
     end
 end
